1beb38a8f539eb8204b80cdd83233921716c5612
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2022-09-02 11:45:39 +0100

    DAC6-1917: Create YourContactDetails page

---
+++ test/controllers/YourContactDetailsControllerSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import matchers.JsonMatchers
+import models.NormalMode
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import play.api.http.Status.OK
+import play.api.libs.json.JsObject
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+import uk.gov.hmrc.viewmodels.NunjucksSupport
+
+import scala.concurrent.Future
+
+class YourContactDetailsControllerSpec extends SpecBase with NunjucksSupport with JsonMatchers {
+
+  def onwardRoute = Call("GET", "/foo")
+
+  lazy val yourContactDetailsRoute = routes.YourContactDetailsController.onPageLoad.url
+
+  "YourContactDetails Controller" - {
+
+    "return OK and the correct view for a GET" in {
+
+      when(mockRenderer.render(any(), any())(any()))
+        .thenReturn(Future.successful(Html("")))
+
+      val application                            = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val request                                = FakeRequest(GET, yourContactDetailsRoute)
+      val templateCaptor: ArgumentCaptor[String] = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor                             = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      templateCaptor.getValue mustEqual "yourContactDetails.njk"
+
+      application.stop()
+    }
+  }
+}
